f4bc45b5601da1aab8e1d44eb3ce0e22b856831e
AnoniempjeVandecasteele <44616744+AnoniempjeVandecasteele@users.noreply.github.com>
2018-11-07 08:46:04 +0100

    Embedded RK methode: RKF45
    Deze functie geeft een lijst terug die alle posities bevat van N aantal deeltjes geëvalueerd over een gegeven aantal iteraties.

---
+++ Embedded_RK.h
@@ -0,0 +1,23 @@
+#ifndef EMBEDDED_RK_H
+#define EMBEDDED_RK_H
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include "3DVectClass.h"
+#include "Embedded_RK.h"
+
+// m = massa	r = positie		N = # deeltjes		i = beschouwde deeltje
+Vec a(std::vector<float> m, std::vector<Vec> r, int i, int N);
+
+// functie RKF45 die posities van deeltjes teruggeeft
+std::vector<std::vector<Vec>> RKF45(std::vector<float> m, std::vector<Vec> r, std::vector<Vec> v, int N, int iteraties, float h);
+
+
+
+
+
+
+#endif
