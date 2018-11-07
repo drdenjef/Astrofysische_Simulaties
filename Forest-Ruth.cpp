0000af4f3c9aa4d6a64db3e89f1f9a5957c3b3fc
TristanDickLover <38313031+TristanDickLover@users.noreply.github.com>
2018-11-07 09:18:57 +0100

    Create Forest-Ruth.cpp
    Aparte file voor Forest-Ruth

---
+++ Forest-Ruth.cpp
@@ -0,0 +1,66 @@
+#include "pch.h"
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+using namespace std;
+#include "3DVectClass.h"
+
+/*****************************************
+*										 *
+*  Forest-Ruth Method   *
+*										 *
+*****************************************/
+
+Vec a(vector<float> m, vector<Vec> r, int i, int N) {
+
+	Vec a;
+
+	for (int j = 0; j < N; j++) {
+		if (j != i) {
+			a += m[j] * (r[i] - r[j]) / (r[i] - r[j]).norm3();
+		}
+
+	}
+	return -1.*a;
+}
+
+vector<vector<Vec>> ForestRuth(vector<Vec> pos, vector<Vec> snel, vector<float> mass, int h, int aantal) {
+	
+	vector<vector<Vec>> finale_pos;
+	float theta = 1 / (2 - pow(2, 1 / 3));
+	
+	// iteratie over het aantal deeltjes
+	for (int k = 0; k < 1; k += h) {
+		for (int i = 0; i < aantal; i += 1) {
+			// substep 1
+			pos[i] = pos[i] + 0.5*theta*h*snel[i];
+			
+			// berekenen van de versnelling
+			Vec acc = a(mass, pos, i, aantal);
+
+			// substeps 2 & 3
+			snel[i] = snel[i] + theta * h*acc;
+			pos[i] = pos[i] + 0.5*(1-theta)*h*snel[i];
+			
+			// berekenen van de versnelling
+			Vec acc = a(mass, pos, i, aantal);
+			
+			//substeps 4 & 5
+			snel[i] = snel[i] + (1-2*theta) * h*acc[i];
+			pos[i] = pos[i] + 0.5*(1 - theta)*h*snel[i];
+			
+			// berekenen van de versnelling
+			Vec acc = a(mass, pos, i, aantal);
+			
+			// substeps 6 & 7
+			snel[i] = snel[i] + theta * h*acc[i];
+			pos[i] = pos[i] + 0.5*theta*h*snel[i];
+		}
+		finale_pos.push_back(pos);
+	}
+
+	return finale_pos;
+}
