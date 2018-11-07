ce27e3ffcbc6407c3a6b6ba6062b6bac2113c3b3
TristanDickLover <38313031+TristanDickLover@users.noreply.github.com>
2018-11-07 09:31:56 +0100

    update output
    nu geeft posities en snelheden terug

--- Forest-Ruth.h
+++ Forest-Ruth.h
@@ -11,5 +11,5 @@ using namespace std;
 // m = massa	r = positie		N = # deeltjes		i = beschouwde deeltje
 Vec a(vector<float> m, vector<Vec> r, int i, int N)
 
-// functie ForestRuth die posities van deeltjes teruggeeft
-vector<vector<Vec>> ForestRuth(vector<Vec> pos, vector<Vec> snel, vector<float> mass, int h, int aantal)
+// functie ForestRuth die posities + snelheden van deeltjes teruggeeft
+vector<vector<vector<Vec>>> ForestRuth(vector<Vec> pos, vector<Vec> snel, vector<float> mass, int h, int aantal)
