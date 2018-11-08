25db55343e004d3f21e8d80b701f43e050db3b0a
EwoudK <38423413+EwoudK@users.noreply.github.com>
2018-11-08 19:25:22 +0000

    Add files via upload

--- Verlet.cpp
+++ Verlet.cpp
@@ -8,7 +8,7 @@
 #include "3DVectClass.h"
 using namespace std;
 
-Vec Acceleration(vector<Vec> posities, vector<float> massas, int aantal_deeltjes, int i){
+Vec Acceleration(vector<Vec> r, vector<float> m, int aantal_deeltjes, int i){
 
   Vec a;
   for ( int j = 0; j < aantal_deeltjes; j++){
@@ -19,18 +19,44 @@ Vec Acceleration(vector<Vec> posities, vector<float> massas, int aantal_deeltjes
   return a;
 }
 
-vector<vector<Vec>> Verlet(vector<Vec> r0, vector<Vec> v0, vector<float> m, int N, int aantal_iteraties, float h){
+vector<vector<vector<Vec>>> Verlet(vector<Vec> r0, vector<Vec> v0, vector<float> m, int N, float h, int I){
 
-  vector<Vec> r;
-  vector<Vec> v;
-  vector<vector<Vec>> pos
-  for( int j = 0; j < aantal_iteraties; j++){
+  vector<Vec> r = r0;
+  vector<Vec> v = v0;
 
-    for ( int i = 0; i < aantal_deeltjes; i++){
-      v[i] = v0[i] + 0.5*h*Acceleration(r, m, N, i);
-      r[i] = r0[i] + h*v[i];
-      v[i] = v[i] + 0.5*h*Acceleration(r, m, N, i);
+  vector<vector<Vec>> pos = { r };
+  vector<vector<Vec>> vel = { v };
 
-    }
+
+  vector<vector<vector<Vec>>> finaal;
+  
+  for(int j =0; j < I; j++){
+      for (int i = 0; i < N; i++) {
+          v[i] = v[i] + 0.5 * h * Acceleration(r, m, N, i);
+          r[i] = r[i] + h * v[i];
+          v[i] = v[i] + 0.5 * h * Acceleration(r, m, N, i);
+
+      }
+      pos.push_back(r);
+      vel.push_back(v);
+
   }
+  return finaal;
+}
+
+// we definieren deze hier nu om onze code te testen
+float h = 0.01;
+Vec x(1., 2., 1.);
+Vec y(2., 1., 3.);
+Vec p(0.2, 0.05, 0.6);
+Vec q(0.8, 0.5, 0.);
+vector <Vec> r0 = { x, y };
+vector <Vec> v0 = { p, q };
+vector <float> m = { 2.1, 1.5 };
+int const N = 2;
+int const I = 10;
+
+int main() {
+   vector<vector<vector<Vec>>> a = Verlet(r0, v0, m, N, h, I);
+    cout << "done";
 }
