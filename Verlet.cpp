d9a33c0a461b3966de102f3e2685c21ddd7387e8
EwoudK <38423413+EwoudK@users.noreply.github.com>
2018-11-14 09:16:48 +0000

    update verlet algoritme

--- Verlet.cpp
+++ Verlet.cpp
@@ -6,29 +6,23 @@
 #include <vector>
 #include <map>
 #include "3DVectClass.h"
+#include "hulpfuncties.h"
 using namespace std;
-
-Vec Acceleration(vector<Vec> r, vector<float> m, int aantal_deeltjes, int i){
-
-  Vec a;
-  for ( int j = 0; j < aantal_deeltjes; j++){
-      if (j != i){
-        a += m[j]*(r[j] - r[i])/(r[i] - r[j]).norm3();
-      }
-  }
-  return a;
 }
 
-vector<vector<vector<Vec>>> Verlet(vector<Vec> r0, vector<Vec> v0, vector<float> m, int N, float h, int I){
+vector<vector<vector<Vec>>> Verlet(vector<Vec> r0, vector<Vec> v0, vector<double> m, int N, float h, int I){
 
   vector<Vec> r = r0;
   vector<Vec> v = v0;
 
   vector<vector<Vec>> pos = { r };
   vector<vector<Vec>> vel = { v };
-
+  double energie0 = Energie(r, v, m);
 
   vector<vector<vector<Vec>>> finaal;
+
+  ofstream outfile("energie!.txt");
+  outfile << setprecision(15);
   
   for(int j =0; j < I; j++){
       for (int i = 0; i < N; i++) {
@@ -36,27 +30,37 @@ vector<vector<vector<Vec>>> Verlet(vector<Vec> r0, vector<Vec> v0, vector<float>
           r[i] = r[i] + h * v[i];
           v[i] = v[i] + 0.5 * h * Acceleration(r, m, N, i);
 
+
       }
       pos.push_back(r);
       vel.push_back(v);
+      double energie = Energie(r, v, m);
+      double energie_error = error_energie(r, v, m, energie0);
+
+      outfile << energie << ' '<< energie_error << '\n';
 
   }
+  finaal.push_back(pos);
+  finaal.push_back(vel);
+  outfile.close();
   return finaal;
 }
 
 // we definieren deze hier nu om onze code te testen
-float h = 0.01;
-Vec x(1., 2., 1.);
-Vec y(2., 1., 3.);
-Vec p(0.2, 0.05, 0.6);
-Vec q(0.8, 0.5, 0.);
+float h = 0.0001;
+Vec x(-0.45, 0., 0.);
+Vec y(0.45, 0., 0.);
+
+Vec p(0., -0.2, 0.);
+Vec q(0., 0.2, 0.);
 vector <Vec> r0 = { x, y };
 vector <Vec> v0 = { p, q };
-vector <float> m = { 2.1, 1.5 };
+vector <double> m = { 0.5, 0.5 };
 int const N = 2;
-int const I = 10;
+int const I = 100000;
 
 int main() {
    vector<vector<vector<Vec>>> a = Verlet(r0, v0, m, N, h, I);
-    cout << "done";
+   wegschrijven(a[0], "orbit", 2);
+   cout << "done";
 }
