958052b0d401f52e0ad8b5b3b0194e183f38aef7
AnoniempjeVandecasteele <44616744+AnoniempjeVandecasteele@users.noreply.github.com>
2018-11-14 08:14:54 +0100

    Add files via upload
    Deze integrator schrijft rechtstreeks de posities en energieën en relatieve fouten op de energieën weg op dezelfde manier als de functie "wegschrijven" die in hulpfuncties gedefinieerd staat. De variabele tijdstap moet ik  nog implementeren.

---
+++ RKF45.h
@@ -0,0 +1,21 @@
+#ifndef RKF45_H
+#define RKF45_H
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include "3DVectClass.h"
+#include "hulpfuncties.h"
+
+
+// functie RKF45 die posities van deeltjes teruggeeft
+void RKF45(std::vector<double> m, std::vector<Vec> r, std::vector<Vec> v, int N, int iteraties, double h);
+
+
+
+
+
+
+#endif
