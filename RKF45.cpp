7a4f33ccefbebde5a90db0c41db3f0602826ce39
AnoniempjeVandecasteele <44616744+AnoniempjeVandecasteele@users.noreply.github.com>
2018-11-14 08:41:09 +0100

    cpp file van RKF45

--- RKF45.cpp
+++ RKF45.cpp
@@ -30,11 +30,11 @@ void RKF45(std::vector<double> m, std::vector<Vec> r, std::vector<Vec> v, int N,
 
 
 
-	// maak een file aan waar de energieën worden bijgehouden
+	// maak een file aan waar de energieÃ«n worden bijgehouden
 	std::ofstream outfile2("RKF45_E.txt");
 	outfile2 << std::setprecision(15);
 
-	// maak een file aan waar de relatieve fouten van de energieën worden bijgehouden
+	// maak een file aan waar de relatieve fouten van de energieÃ«n worden bijgehouden
 	std::ofstream outfile3("RKF45_E_err.txt");
 	outfile3 << std::setprecision(15);
 
@@ -45,6 +45,8 @@ void RKF45(std::vector<double> m, std::vector<Vec> r, std::vector<Vec> v, int N,
 	// kan meegegeven worden aan de functie RKF45
 	for (int i = 0; i < iteraties; i++) {
 
+		double h_var = variabele_h(h, r);
+
 		// begint te itereren over alle deeltjes
 		for (int j = 0; j < N; j++) {
 			
@@ -54,29 +56,29 @@ void RKF45(std::vector<double> m, std::vector<Vec> r, std::vector<Vec> v, int N,
 			//r[j] = r[j]
 			Vec kv1 = -1*a(m, r, j, N);
 
-			Vec kr2 = v[j] + .25*h*kv1;
-			r[j] = r[j] + .25*h*kr1;
+			Vec kr2 = v[j] + .25*h_var*kv1;
+			r[j] = r[j] + .25*h_var*kr1;
 			Vec kv2 = -1*a(m, r, j, N);
 
-			Vec kr3 = v[j] + (3 / 8)*h*kv2;
-			r[j] = r[j] + (3 / 8)*h*kr2;
+			Vec kr3 = v[j] + (3 / 8)*h_var*kv2;
+			r[j] = r[j] + (3 / 8)*h_var*kr2;
 			Vec kv3 = -1*a(m, r, j, N);
 
-			Vec kr4 = v[j] + (12 / 13)*h*kv3;
-			r[j] = r[j] + (12 / 13)*h*kr3;
+			Vec kr4 = v[j] + (12 / 13)*h_var*kv3;
+			r[j] = r[j] + (12 / 13)*h_var*kr3;
 			Vec kv4 = -1*a(m, r, j, N);
 
-			Vec kr5 = v[j] + h * kv4;
-			r[j] = r[j] + h * kr4;
+			Vec kr5 = v[j] + h_var * kv4;
+			r[j] = r[j] + h_var * kr4;
 			Vec kv5 = -1*a(m, r, j, N);
 
-			Vec kr6 = v[j] + .5*h*kv5;
-			r[j] = r[j] + .5*h*kr5;
+			Vec kr6 = v[j] + .5*h_var*kv5;
+			r[j] = r[j] + .5*h_var*kr5;
 			Vec kv6 = -1*a(m, r, j, N);
 
 			// bereken r_n+1 en v_n+1 en onthoud deze woorden voor volgende iteraties
-			r[j] = r[j] + h * ((25 / 216)*kr1 + (1408 / 2565)*kr3 + (2197 / 4104)*kr4 - .2*kr5);
-			v[j] = v[j] + h * ((25 / 216)*kv1 + (1408 / 2565)*kv3 + (2197 / 4104)*kv4 - .2*kv5);
+			r[j] = r[j] + h_var * ((25 / 216)*kr1 + (1408 / 2565)*kr3 + (2197 / 4104)*kr4 - .2*kr5);
+			v[j] = v[j] + h_var * ((25 / 216)*kv1 + (1408 / 2565)*kv3 + (2197 / 4104)*kv4 - .2*kv5);
 
 			
 			outfile1 << r[j].x() << ' ' << r[j].y() << ' ' << r[j].z() << '\t';
