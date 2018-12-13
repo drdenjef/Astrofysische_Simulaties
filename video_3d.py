ff05cfdab9978e80d4408b5f0a3ad3395a90d17b
ozzy0 <44637998+ozzy0@users.noreply.github.com>
2018-12-13 19:35:06 +0100

    update: filling bar

--- video_3d.py
+++ video_3d.py
@@ -3,59 +3,64 @@ import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.animation as animation
 
-#textbestanden inlezen
-x1 = np.loadtxt("C:\\Users\\Osman\\Downloads\\Burrau_RK4.txt", unpack=True)[0][::50]
-y1 = np.loadtxt("C:\\Users\\Osman\\Downloads\\Burrau_RK4.txt", unpack=True)[1][::50]
-z1 = np.loadtxt("C:\\Users\\Osman\\Downloads\\Burrau_RK4.txt", unpack=True)[2][::50]
 
-x2 = np.loadtxt("C:\\Users\\Osman\\Downloads\\Burrau_RK4.txt", unpack=True)[3][::50]
-y2 = np.loadtxt("C:\\Users\\Osman\\Downloads\\Burrau_RK4.txt", unpack=True)[4][::50]
-z2 = np.loadtxt("C:\\Users\\Osman\\Downloads\\Burrau_RK4.txt", unpack=True)[5][::50]
+x1 = np.loadtxt("C:\\Users\\Osman\\Downloads\\burrau_FR.txt", unpack=True)[0][::100]
+y1 = np.loadtxt("C:\\Users\\Osman\\Downloads\\burrau_FR.txt", unpack=True)[1][::100]
+z1 = np.loadtxt("C:\\Users\\Osman\\Downloads\\burrau_FR.txt", unpack=True)[2][::100]
 
-x3 = np.loadtxt("C:\\Users\\Osman\\Downloads\\Burrau_RK4.txt", unpack=True)[6][::50]
-y3 = np.loadtxt("C:\\Users\\Osman\\Downloads\\Burrau_RK4.txt", unpack=True)[7][::50]
-z3 = np.loadtxt("C:\\Users\\Osman\\Downloads\\Burrau_RK4.txt", unpack=True)[8][::50]
+x2 = np.loadtxt("C:\\Users\\Osman\\Downloads\\burrau_FR.txt", unpack=True)[3][::100]
+y2 = np.loadtxt("C:\\Users\\Osman\\Downloads\\burrau_FR.txt", unpack=True)[4][::100]
+z2 = np.loadtxt("C:\\Users\\Osman\\Downloads\\burrau_FR.txt", unpack=True)[5][::100]
 
+x3 = np.loadtxt("C:\\Users\\Osman\\Downloads\\burrau_FR.txt", unpack=True)[6][::100]
+y3 = np.loadtxt("C:\\Users\\Osman\\Downloads\\burrau_FR.txt", unpack=True)[7][::100]
+z3 = np.loadtxt("C:\\Users\\Osman\\Downloads\\burrau_FR.txt", unpack=True)[8][::100]
+
+
+time = np.loadtxt("C:\\Users\\Osman\\Downloads\\burrau_FR_E_err.txt", unpack=True)[2][::100]
 
 
 fig = plt.figure()
-ax = p3.Axes3D(fig)
+ax1 = plt.subplot2grid((1,21),(0,0), rowspan = 1, colspan = 19,projection="3d")
+ax1.set_xlim(-5,5)
+ax1.set_ylim(-5,5)
+ax1.set_zlim(-5,5)
+ax1.set_xlabel("x")
+ax1.set_ylabel("y")
+ax1.set_zlabel("z")
+ax3 = plt.subplot2grid((1,21),(0,20),rowspan = 1, colspan = 1)
+ax3.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
+ax3.tick_params(axis='y', which='both', left=False, right=True, labelleft = False, labelright = True)
+ax3.set_ylim(0,time[-1])
+ax3.set_ylabel('t')
+ax3.yaxis.set_label_position("right")
 
-# stop voor elke punt de coordinaten hiering
+
+
 data = np.array([[x1,y1,z1],[x2,y2,z2],[x3,y3,z3]])
+lines = [ax1.plot(dat[0, 0:1], dat[1, 0:1], dat[2, 0:1])[0] for dat in data]
+bar, = ax3.bar(0,0)
 
-lines = [ax.plot(dat[0, 0:1], dat[1, 0:1], dat[2, 0:1])[0] for dat in data]
-
-
-ax.set_xlim(-5,5)
-ax.set_ylim(-5,5)
-ax.set_zlim(-5,5)
-ax.set_xlabel("x")
-ax.set_ylabel("y")
-ax.set_zlabel("z")
 plt.rcParams['animation.html'] = 'html5'
 
-#update functies, ik weet eingenlijk niet hoe dit werk.
-# de lengte van de trails kan je aanpassen door gewoon het getal 50 door iets anders te veranderen
 def update_lines(num, dataLines, lines):
-    # deze plot is constant aan het roteren, verwijder gewoon deze 2 lijnen als je een steady plot wilt
-    angle = num % 360
-    ax.view_init(30, angle)
     for line, data in zip(lines, dataLines):
         plt.draw()
         if num >50:
             line.set_data(data[0:2, num-50:num])
             line.set_3d_properties(data[2, num-50:num])
             line.set_marker(",")
+
         else:
             line.set_data(data[0:2, :num])
             line.set_3d_properties(data[2, :num])
             line.set_marker(",")
+        ###
+        bar.set_height(time[num])
     return lines
 
-#hier wordt de uiteindelijke video gemaakt
+
 line_ani = animation.FuncAnimation(fig, update_lines, len(x1), fargs=(data, lines),
                                    interval=100, blit=True, repeat=True)
 
-#hier kan de fps van de video aanpassen en de video opslaan
-line_ani.save('burrau_3d.mp4', writer='ffmpeg',fps=30)
+line_ani.save('burrau_3d.mp4', writer='ffmpeg',fps=50)
